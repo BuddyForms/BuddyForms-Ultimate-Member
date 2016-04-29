801af6276937ccf0e0b6de13090bd6b839e42ea3
Sven Lehnert <mail@sven-lehnert.de>
2016-04-29 08:59:36 +0200

    BuddyForms Ultimate Member Integration first working version.

--- loader.php
+++ loader.php
@@ -29,21 +29,116 @@
  ****************************************************************************
  */
 
+
+ function buddyforms_ultimate_members_admin_settings_sidebar_metabox(){
+     add_meta_box('buddyforms_ultimate_members', __("Ultimate Members",'buddyforms'), 'buddyforms_ultimate_members_admin_settings_sidebar_metabox_html', 'buddyforms', 'side', 'low');
+ }
+
+ function buddyforms_ultimate_members_admin_settings_sidebar_metabox_html(){
+     global $post, $buddyforms;
+
+     if($post->post_type != 'buddyforms')
+         return;
+
+     $buddyform = get_post_meta(get_the_ID(), '_buddyforms_options', true);
+
+
+     $form_setup = array();
+
+     $ultimate_members_profiles_integration = '';
+     if(isset($buddyform['ultimate_members_profiles_integration']))
+         $ultimate_members_profiles_integration = $buddyform['ultimate_members_profiles_integration'];
+
+     $ultimate_members_profiles_parent_tab = false;
+     if(isset($buddyform['ultimate_members_profiles_parent_tab']))
+         $ultimate_members_profiles_parent_tab = $buddyform['ultimate_members_profiles_parent_tab'];
+
+     $form_setup[] = new Element_Checkbox("<b>" . __('Add this form as Profile Tab', 'buddyforms') . "</b>", "buddyforms_options[ultimate_members_profiles_integration]", array("integrate" => "Integrate this Form"), array('value' => $ultimate_members_profiles_integration, 'shortDesc' => __('Many forms can share the same attached page. All Forms with the same attached page can be grouped together with this option. All Forms will be listed as sub nav tabs of the page main nav', 'buddyforms')));
+     $form_setup[] = new Element_Checkbox("<br><b>" . __('Use Attached Page as Parent Tab and make this form a sub tab of the parent', 'buddyforms') . "</b>", "buddyforms_options[ultimate_members_profiles_parent_tab]", array("attached_page" => "Use Attached Page as Parent"), array('value' => $ultimate_members_profiles_parent_tab, 'shortDesc' => __('Many Forms can have the same attached Page. All Forms with the same page with page as parent enabled will be listed as sub forms. This why you can group forms.', 'buddyforms')));
+     //$form_setup[] = new Element_Checkbox("<br><b>" . __('Hide Post List', 'buddyforms') . "</b>", "buddyforms_options[profiles_parent_tab]", array("hide" => "Hide"), array('value' => $profiles_parent_tab, 'shortDesc' => __('Can be useful if you want to display all posts in one tab and only separate the forms', 'buddyforms')));
+
+     foreach($form_setup as $key => $field){
+         echo '<div class="buddyforms_field_label">' . $field->getLabel() . '</div>';
+         echo '<div class="buddyforms_field_description">' . $field->getShortDesc() . '</div>';
+         echo '<div class="buddyforms_form_field">' . $field->render() . '</div>';
+     }
+ }
+ add_filter('add_meta_boxes','buddyforms_ultimate_members_admin_settings_sidebar_metabox');
+
+
 /* add a custom tab to show user pages */
 add_filter('um_profile_tabs', 'bf_pages_tab', 1000 );
 function bf_pages_tab( $tabs ) {
-    $tabs['pages'] = array(
-        'name' => 'Pages',
-        'icon' => 'um-faicon-pencil',
-        'count' => 0,
-        'custom' => true
-    );
-    return $tabs;
+  global $buddyforms;
+  if(isset($buddyforms)) : foreach($buddyforms as $form_slug => $form){
+    if(isset($form['ultimate_members_profiles_integration'])){
+        $tabs[$form_slug] = array(
+            'name' => $form['name'],
+            'icon' => 'um-faicon-pencil',
+            'subnav' => array(
+              'posts' => __('View','buddyforms'),
+              'form' => __('Create','buddyforms'),
+            ),
+            'subnav_default' => 'posts',
+            'custom' => true
+        );
+        add_action('um_profile_content_' . $form_slug . '_default', create_function('$form_slug', 'bf_um_profile_form_test('.$form_slug.');'));
+        add_action('um_profile_content_' . $form_slug . '_posts', create_function('$form_slug', 'bf_um_profile_form_test('.$form_slug.');'));
+        add_action('um_profile_content_' . $form_slug . '_form', create_function('$form_slug', 'bf_um_profile_form_test('.$form_slug.');'));
+
+      }
+    }
+  endif;
+  return $tabs;
 }
 
-/* Tell the tab what to display */
-add_action('um_profile_content_pages_default', 'bf_um_profile_form');
-function bf_um_profile_form( $args ) {
-    global $ultimatemember;
-    echo do_shortcode('[buddyforms_form form_slug="tests"]');
+
+function bf_um_profile_form_test($form_slug){
+  // echo '<pre>';
+  // print_r($form_slug);
+  // echo '</pre>';
+  echo 'sadasd'.$_GET['profiletab'];
+  if(isset($_GET['profiletab']) && $_GET['profiletab'] == $form_slug ){
+    if(isset($_GET['subnav']) && $_GET['subnav'] == 'posts')  {
+      echo do_shortcode('[buddyforms_the_loop form_slug="'.$form_slug.'"]');
+    } else {
+      echo do_shortcode('[buddyforms_form form_slug="'.$form_slug.'"]');
+    }
+  }
 }
+
+function bf_um_front_js_css_loader($fount){
+    return true;
+}
+add_filter('buddyforms_front_js_css_loader', 'bf_um_front_js_css_loader', 10, 1 );
+
+
+//
+// Redirect to  single posts list, if form settup -> after submit is set to display posts list
+//
+function bf_um_after_save_post_redirect($permalink){
+  global $buddyforms, $ultimatemember, $post;
+
+  $um_options = get_option('um_options');
+
+print_r($_POST);
+
+echo $permalink;
+
+  if(isset($um_options['core_user']) && $um_options['core_user'] == $post->ID)  {
+    //$permalink = get_the_permalink($post->ID) . '?profiletab=product';
+  }
+
+
+  // if (isset($buddyforms[$_POST['form_slug']]['after_submit'])) {
+  //     if ($buddyforms[$_POST['form_slug']]['after_submit'] == 'display_posts_list') {
+  //         $permalink = bp_get_group_permalink() . bp_current_action();
+  //     }
+  // }
+
+
+
+return $permalink;
+
+}
+add_filter('buddyforms_after_save_post_redirect', 'bf_um_after_save_post_redirect', 10 ,1);
