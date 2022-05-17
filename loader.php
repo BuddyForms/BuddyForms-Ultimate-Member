562508bd489f6cb7feddfb93c9a9ff84e3ea2c8c
Romeli Rodriguez <romelirodriguez@gmail.com>
2022-05-16 23:14:20 -0400

    New version: 1.3.13

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
  * Plugin Name: BuddyForms Ultimate Member
  * Plugin URI: https://themekraft.com/products/ultimate-member/
  * Description: Extend Ultimate Member Profiles with BuddyForms
- * Version: 1.3.12
+ * Version: 1.3.13
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/buddyforms/
  * License: GPLv2 or later
